161b4edea495106544970062b6237185a78401ce
MarianKijewski <mariankijewski@outlook.com>
2025-11-04 14:58:41 +0100

    design: remove social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,8 +59,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       colorMode: {
         respectPrefersColorScheme: true,
       },
