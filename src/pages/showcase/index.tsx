38641794a7155d75875239e14eb9d967281013dd
MarianKijewski <mariankijewski@outlook.com>
2025-11-06 12:40:07 +0100

    remove call to action about adding own content

--- src/pages/showcase/index.tsx
+++ src/pages/showcase/index.tsx
@@ -20,18 +20,12 @@ const TITLE = translate({message: 'Docusaurus Site Showcase'});
 const DESCRIPTION = translate({
   message: 'List of websites people are building with Docusaurus',
 });
-const SUBMIT_URL = 'https://github.com/facebook/docusaurus/discussions/7826';
 
 function ShowcaseHeader() {
   return (
     <section className="margin-top--lg margin-bottom--lg text--center">
       <Heading as="h1">{TITLE}</Heading>
       <p>{DESCRIPTION}</p>
-      <Link className="button button--primary" to={SUBMIT_URL}>
-        <Translate id="showcase.header.button">
-          🙏 Please add your site
-        </Translate>
-      </Link>
     </section>
   );
 }
