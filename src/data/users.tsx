d43327890a082ba9a36b1afe54c9a4258d410712
MarianKijewski <mariankijewski@outlook.com>
2025-11-06 12:54:22 +0100

    remove prompt comment to add App

--- src/data/users.tsx
+++ src/data/users.tsx
@@ -10,30 +10,6 @@
 import {translate} from '@docusaurus/Translate';
 import {sortBy} from '@site/src/utils/jsUtils';
 
-/*
- * ADD YOUR SITE TO THE DOCUSAURUS SHOWCASE
- *
- * Please don't submit a PR yourself: use the Github Discussion instead:
- * https://github.com/facebook/docusaurus/discussions/7826
- *
- * Instructions for maintainers:
- * - Add the site in the json array below
- * - `title` is the project's name (no need for the "Docs" suffix)
- * - A short (≤120 characters) description of the project
- * - Use relevant tags to categorize the site (read the tag descriptions on the
- *   https://docusaurus.io/showcase page and some further clarifications below)
- * - Add a local image preview (decent screenshot of the Docusaurus site)
- * - The image MUST be added to the GitHub repository, and use `require("img")`
- * - The image has to have minimum width 640 and an aspect of no wider than 2:1
- * - If a website is open-source, add a source link. The link should open
- *   to a directory containing the `docusaurus.config.js` file
- * - Resize images: node admin/scripts/resizeImage.js
- * - Run optimizt manually (see resize image script comment)
- * - Open a PR and check for reported CI errors
- *
- * Example PR: https://github.com/facebook/docusaurus/pull/7620
- */
-
 // LIST OF AVAILABLE TAGS
 // Available tags to assign to a showcase site
 // Please choose all tags that you think might apply.
