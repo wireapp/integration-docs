cf52b9236981899fb8956cfcdc794647b24fecd3
MarianKijewski <mariankijewski@outlook.com>
2026-07-09 11:57:29 +0200

    Add copy from wire.com adjusted for our use
    The styling is added to preserve newlines in description.

--- src/scripts/cookie_consent.js
+++ src/scripts/cookie_consent.js
@@ -37,38 +37,32 @@ export default function CookieConsentBanner() {
         translations: {
           en: {
             consentModal: {
-              title: "Hello traveller, it's cookie time!",
-              description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-              acceptAllBtn: "Accept all",
-              acceptNecessaryBtn: "Reject all",
-              showPreferencesBtn: "Manage preferences",
-              footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+              description: "This website stores cookies on your computer. These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media. To find out more about the cookies we use, see our Privacy Policy.\n\n" +
+                "If you decline, your information won’t be tracked when you visit this website. A single cookie will be used in your browser to remember your preference not to be tracked.",
+              acceptAllBtn: "Accept",
+              acceptNecessaryBtn: "Decline",
+              showPreferencesBtn: "Cookie settings",
+              footer: "<a href=\"#link\">Privacy Policy</a>"
             },
             preferencesModal: {
-              title: "Consent Preferences Center",
-              acceptAllBtn: "Accept all",
-              acceptNecessaryBtn: "Reject all",
-              savePreferencesBtn: "Save preferences",
+              title: "About Cookies",
+              acceptAllBtn: "Accept All",
+              savePreferencesBtn: "Save settings",
               closeIconLabel: "Close modal",
               serviceCounterLabel: "Service|Services",
               sections: [
                 {
-                  title: "Cookie Usage",
-                  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  description: "This site uses cookies. We use cookies mainly to improve and analyze your experience on our websites. Because we respect your right to privacy, you can choose not to allow some types of cookies. Click on the different category headings to find out more and change your default settings."
                 },
                 {
-                  title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
-                  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                  title: "Necessary <span class=\"pm__badge\">Always active</span>",
+                  description: "These cookies are necessary for the website to function and cannot be switched off in our systems. They are usually only set in response to actions made by you which amount to a request for services, such as setting your privacy preferences or filling in forms",
                   linkedCategory: "necessary"
                 },
                 {
-                  title: "Analytics Cookies",
-                  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                  title: "Analytics",
+                  description: "These cookies help us to understand how visitors engage with the website. We may use a set of cookies to collect information and report site usage statistics.",
                   linkedCategory: "analytics"
-                },
-                {
-                  title: "More information",
-                  description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
                 }
               ]
             }
